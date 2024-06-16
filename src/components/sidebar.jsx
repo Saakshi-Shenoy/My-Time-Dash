4ec3bc7afff6826922e935fd33f021b296f293ff
Saakshi-Shenoy <shenoy.saakshi910@gmail.com>
2024-06-16 12:36:12 +0530

    small changes

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Sidebar } from 'flowbite-react';
-import { HiChartPie } from 'react-icons/hi';
-import {Link} from "react-router-dom";
-import '../css/side.css';
+import React from "react";
+import { Sidebar } from "flowbite-react";
+import { HiChartPie } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import "../css/side.css";
 
 function SidebarMenu() {
   return (
-    <Sidebar className="sidebar" aria-label="Sidebar with multi-level dropdown example">
+    <Sidebar
+      className="sidebar"
+      aria-label="Sidebar with multi-level dropdown example"
+    >
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-
-         <Sidebar.Item as={Link} to="/" icon={HiChartPie}>
+          <Sidebar.Item as={Link} to="/" icon={HiChartPie}>
             Dashboard
           </Sidebar.Item>
 
           <Link to="/about">
-          <button className='px-5 text-gray-700'>
-            About Us
-          </button>
+            <button className="text-gray-700" style={{ paddingLeft: "23%" }}>
+              About Us
+            </button>
           </Link>
-
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
